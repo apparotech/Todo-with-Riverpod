db3ec3a4d4b99e42bd7ab0bffcaae5e46474ad08
Raj-mahto <raj.mahto@rthetapi.com>
2025-09-11 23:32:08 +0530

    add

--- lib/provider/category_provider.dart
+++ lib/provider/category_provider.dart
@@ -5,3 +5,11 @@ import 'package:todo_application/utils/TaskCategory.dart';
 final categoryProvider  = StateProvider.autoDispose<TaskCategory>((ref) {
   return TaskCategory.others;
 });
+
+final categoryProviders  = StateProvider.autoDispose<TaskCategory>((ref) {
+  return TaskCategory.others;
+});
+
+final categoryProviderss  = StateProvider.autoDispose<TaskCategory>((ref) {
+  return TaskCategory.others;
+});
